13edfad5fe84ffa341448a2854926202a16def14
Space-Belt <dngurwn@gmail.com>
2025-04-23 10:42:45 +0900

    feat: Drawer 스크린 뷰 추가

---
+++ app/src/main/java/com/example/musicappui/ui/theme/MainView.kt
@@ -0,0 +1,117 @@
+package com.example.musicappui.ui.theme
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.musicappui.Screen
+import com.example.musicappui.screensInDrawer
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainView() {
+
+    val scaffoldState: ScaffoldState = rememberScaffoldState()
+
+    val scope: CoroutineScope = rememberCoroutineScope()
+
+    // 어떤 화면에 있는지 알기
+    val controller: NavController = rememberNavController()
+    val navBackStackEntry by controller.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    val title = remember{
+        mutableStateOf("")
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {Text("Home")},
+                navigationIcon = {
+                    IconButton(onClick = {
+                        // 드로어 열기
+                        scope.launch {
+                            scaffoldState.drawerState.open()
+                        }
+                    }) {
+                        Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Menu")
+                    }}
+            )
+        }, scaffoldState = scaffoldState,
+        drawerContent = {
+            LazyColumn(Modifier.padding(16.dp)) {
+                items(screensInDrawer) {
+                    item ->
+                    DrawerItem(selected = currentRoute == item.dRoute, item = item) {
+                        scope.launch{
+                            scaffoldState.drawerState.close()
+                        }
+                        if(item.dRoute == "add_account") {
+
+                        } else {
+                            controller.navigate(item.dRoute)
+                            title.value = item.dTitle
+                        }
+                    }
+                }
+            }
+        }
+    ) {
+
+        Text("Text", modifier = Modifier.padding(it))
+    }
+}
+
+@Composable
+fun DrawerItem(
+    selected:Boolean,
+    item: Screen.DrawerScreen,
+    onDrawerItemClicked: () -> Unit
+) {
+    val background = if (selected) Color.DarkGray else Color.White
+    Row (
+        Modifier.fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 16.dp).background(background)
+            .clickable {
+                onDrawerItemClicked()
+            }) {
+        Icon(
+            painter = painterResource(id = item.icon),
+            contentDescription = item.dTitle,
+            Modifier.padding(end = 8.dp, top = 4.dp)
+        )
+        Text(
+            text = item.dTitle,
+            style = MaterialTheme.typography.h5
+        )
+    }
+}
+
